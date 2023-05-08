645ba24fc19e4a599cb6cd13ad4dd939569da2ac
Le Cong Tuan Anh <lecongdev123@gmail.com>
2023-05-08 21:12:53 +0700

    Lan dau

--- codeJs.js
+++ codeJs.js
@@ -1,162 +1,64 @@
-var wakeuptime = 7;
-var noon = 12;
-var lunchtime = 12;
-var naptime = lunchtime + 2;
-var partytime;
-var evening = 18;
+var enterButton = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+var item = document.getElementsByTagName("li");
 
-// Getting it to show the current time on the page
-var showCurrentTime = function()
-{
-    // display the string on the webpage
-    var clock = document.getElementById('clock');
- 
-    var currentTime = new Date();
- 
-    var hours = currentTime.getHours();
-    var minutes = currentTime.getMinutes();
-    var seconds = currentTime.getSeconds();
-    var meridian = "AM";
- 
-    // Set hours
-    if (hours >= noon)
-    {
-      meridian = "PM";
-    }
+function inputLength(){
+	return input.value.length;
+} 
 
-    if (hours > noon)
-    {
-      hours = hours - 12;
-    }
- 
-    // Set Minutes
-    if (minutes < 10)
-    {
-        minutes = "0" + minutes;
-    }
- 
-    // Set Seconds
-    if (seconds < 10)
-    {
-        seconds = "0" + seconds;
-    }
- 
-    // put together the string that displays the time
-    var clockTime = hours + ':' + minutes + ':' + seconds + " " + meridian + "!";
- 
-    clock.innerText = clockTime;
-};
+function listLength(){
+	return item.length;
+}
 
-// Getting the clock to increment on its own and change out messages and pictures
-var updateClock = function() 
-{
-  var time = new Date().getHours();
-  var messageText;
-  var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
-
-  var timeEventJS = document.getElementById("timeEvent");
-  var lolcatImageJS = document.getElementById('lolcatImage');
-  
-  if (time == partytime)
-  {
-    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/partyTime.jpg";
-    messageText = "Let's party!";
-  }
-  else if (time == wakeuptime)
-  {
-    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
-    messageText = "Wake up!";
-  }
-  else if (time == lunchtime)
-  {
-    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
-    messageText = "Let's have some lunch!";
-  }
-  else if (time == naptime)
-  {
-    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
-    messageText = "Sleep tight!";
-  }
-  else if (time < noon)
-  {
-    image = "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg";
-    messageText = "Good morning!";
-  }
-  else if (time >= evening)
-  {
-    image = "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cat_sleep.jpg";
-    messageText = "Good evening!";
-  }
-  else
-  {
-    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
-    messageText = "Good afternoon!";
-  }
-
-  console.log(messageText); 
-  timeEventJS.innerText = messageText;
-  lolcatImage.src = image;
-  
-  showCurrentTime();
-};
-updateClock();
-
-// Getting the clock to increment once a second
-var oneSecond = 1000;
-setInterval( updateClock, oneSecond);
+function createListElement() {
+	var li = document.createElement("li"); // creates an element "li"
+	li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
+	ul.appendChild(li); //adds li to ul
+	input.value = ""; //Reset text input field
 
 
-// Getting the Party Time Button To Work
-var partyButton = document.getElementById("partyTimeButton");
+	//START STRIKETHROUGH
+	// because it's in the function, it only adds it for new items
+	function crossOut() {
+		li.classList.toggle("done");
+	}
 
-var partyEvent = function()
-{
-    if (partytime < 0) 
-    {
-        partytime = new Date().getHours();
-        partyTimeButton.innerText = "Party Over!";
-        partyTimeButton.style.backgroundColor = "#0A8DAB";
-    }
-    else
-    {
-        partytime = -1;
-        partyTimeButton.innerText = "Party Time!";
-        partyTimeButton.style.backgroundColor = "#222";
-    }
-};
-
-partyButton.addEventListener("click", partyEvent);
-partyEvent(); 
+	li.addEventListener("click",crossOut);
+	//END STRIKETHROUGH
 
 
-// Activates Wake-Up selector
-var wakeUpTimeSelector =  document.getElementById("wakeUpTimeSelector");
-
-var wakeUpEvent = function()
-{
-    wakeuptime = wakeUpTimeSelector.value;
-};
-
-wakeUpTimeSelector.addEventListener("change", wakeUpEvent);
+	// START ADD DELETE BUTTON
+	var dBtn = document.createElement("button");
+	dBtn.appendChild(document.createTextNode("X"));
+	li.appendChild(dBtn);
+	dBtn.addEventListener("click", deleteListItem);
+	// END ADD DELETE BUTTON
 
 
-// Activates Lunch selector
-var lunchTimeSelector =  document.getElementById("lunchTimeSelector");
-
-var lunchEvent = function()
-{
-    lunchtime = lunchTimeSelector.value;
-};
-
-lunchTimeSelector.addEventListener("change", lunchEvent);
+	//ADD CLASS DELETE (DISPLAY: NONE)
+	function deleteListItem(){
+		li.classList.add("delete")
+	}
+	//END ADD CLASS DELETE
+}
 
 
-// Activates Nap-Time selector
-var napTimeSelector =  document.getElementById("napTimeSelector");
+function addListAfterClick(){
+	if (inputLength() > 0) { //makes sure that an empty input field doesn't create a li
+		createListElement();
+	}
+}
 
-var napEvent = function()
-{
-    naptime = napTimeSelector.value;
-};
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.which ===13) { //this now looks to see if you hit "enter"/"return"
+		//the 13 is the enter key's keycode, this could also be display by event.keyCode === 13
+		createListElement();
+	} 
+}
 
-napTimeSelector.addEventListener("change", napEvent);
+
+enterButton.addEventListener("click",addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
